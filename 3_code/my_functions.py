89f536beea649955257e91587017afc3981f8791
MaximilianProll <maximilian.proll@hotmail.de>
2018-12-13 10:39:02 +0200

    change names of variables

--- 3_code/my_functions.py
+++ 3_code/my_functions.py
@@ -4,14 +4,14 @@ import os.path
 from keras import backend as K
 
 
-def preprocess_df(_path_to_csv, path_to_data, colour_band, file_extension):
-    _df = pd.read_csv(_path_to_csv)
-    print('successfully loaded file: ', _path_to_csv)
+def preprocess_df(path_to_csv, path_to_data, colour_band, file_extension):
+    df = pd.read_csv(path_to_csv)
+    print('successfully loaded file: ', path_to_csv)
 
     # fill NaN as 0
-    _df = _df.fillna(0)
+    df = df.fillna(0)
 
-    _df = _df.rename(index=str, columns={
+    df = df.rename(index=str, columns={
         'vuosi': 'YEAR',
         'lohkonro': 'field parcel',
         'tunnus': 'identifier',
@@ -24,65 +24,65 @@ def preprocess_df(_path_to_csv, path_to_data, colour_band, file_extension):
         'ositt_tuho': 'partial crop loss'})
 
     # remove duplicated entries in field parcel
-    # print(_df.shape[0] - len(np.unique(_df['field parcel'])), 'duplicate entries')
-    fieldparcel = _df['field parcel']
-    _df = _df[fieldparcel.duplicated() == False]
-    # print(_df.shape[0] - len(np.unique(_df['field parcel'])), 'duplicate entries')
+    # print(df.shape[0] - len(np.unique(df['field parcel'])), 'duplicate entries')
+    fieldparcel = df['field parcel']
+    df = df[fieldparcel.duplicated() == False]
+    # print(df.shape[0] - len(np.unique(df['field parcel'])), 'duplicate entries')
 
-    # print('total number of fields: ', _df.shape[0])
+    # print('total number of fields: ', df.shape[0])
 
     # print('create new column: relative crop loss = crop loss / Property area')
-    _df['full crop loss scaled'] = _df['full crop loss'] / _df['Property area']
-    _df['partial crop loss scaled'] = _df['partial crop loss'] / _df['Property area']
+    df['full crop loss scaled'] = df['full crop loss'] / df['Property area']
+    df['partial crop loss scaled'] = df['partial crop loss'] / df['Property area']
 
     # select largest number of samples for one given plant species
-    plants = _df['PLANT']
+    plants = df['PLANT']
     num = 0
     for plant in np.unique(list(plants)):
-        num_tmp = len(_df[plants == plant])
+        num_tmp = len(df[plants == plant])
         # print(plant, '\t ', num_tmp)
 
         if num_tmp > num:
             num = num_tmp
             plant_max = plant
     # print('maximum number for', plant_max, 'with', num, 'entries')
-    _df = _df[plants == plant_max]
+    df = df[plants == plant_max]
 
     col_list = ['field parcel', 'full crop loss scaled', 'partial crop loss scaled']
 
     # print('trim data frame to:', col_list)
-    _df = _df[col_list]
+    df = df[col_list]
 
-    print('total number of fields before verifying file existence ', _df.shape[0])
+    print('total number of fields before verifying file existence ', df.shape[0])
     # check if files for fields exist, if not, remove from data frame
     not_existing = []
-    for index, row in _df.iterrows():
+    for index, row in df.iterrows():
         file = path_to_data + row['field parcel'] + '_' + colour_band + file_extension
         if not os.path.isfile(file):
             not_existing.append(index)
 
-    _df = _df.drop(not_existing)
+    df = df.drop(not_existing)
 
-    print('data frame created with a total number of fields: ', _df.shape[0])
-    return _df
+    print('data frame created with a total number of fields: ', df.shape[0])
+    return df
 
 
-def split_dataframe(_df, train_p, val_p, random_state=200):
+def split_dataframe(df, train_p, val_p, random_state=200):
     # split data frame into train, validation and test
-    _train_df = _df.sample(frac=train_p, random_state=random_state)
-    _df = _df.drop(_train_df.index)
-    _val_df = _df.sample(frac=val_p / (1 - train_p), random_state=random_state)
-    _test_df = _df.drop(_val_df.index)
+    train_df = df.sample(frac=train_p, random_state=random_state)
+    df = df.drop(train_df.index)
+    val_df = df.sample(frac=val_p / (1 - train_p), random_state=random_state)
+    test_df = df.drop(val_df.index)
 
     # make field parcel as indeces
-    _train_df = _train_df.set_index('field parcel')
-    _val_df = _val_df.set_index('field parcel')
-    _test_df = _test_df.set_index('field parcel')
+    train_df = train_df.set_index('field parcel')
+    val_df = val_df.set_index('field parcel')
+    test_df = test_df.set_index('field parcel')
 
-    return _train_df, _val_df, _test_df
+    return train_df, val_df, test_df
 
 
-def sampling(_args):
+def sampling(args):
     """Reparameterization trick by sampling fr an isotropic unit Gaussian.
     reparameterization trick
     instead of sampling from Q(z|X), sample eps = N(0,I)
@@ -95,9 +95,9 @@ def sampling(_args):
         z (tensor): sampled latent vector
     """
 
-    _z_mean, _z_log_var = _args
-    batch = K.shape(_z_mean)[0]
-    dim = K.int_shape(_z_mean)[1]
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
     # by default, random_normal has mean=0 and std=1.0
     epsilon = K.random_normal(shape=(batch, dim))
-    return _z_mean + K.exp(0.5 * _z_log_var) * epsilon
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
