51cae77755e8280712d6a8bac5f2d94ee19e8f14
MaximilianProll <maximilian.proll@hotmail.de>
2019-02-14 17:36:14 +0200

    update triton bash script for new python file (tf_vae_tfrecords.py) and copying of .tfrecords instead of tar files

---
+++ 3_code/run_tf_vae_tfrecords.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+# request resources
+# -----------------------
+#SBATCH --time=0-12:00:00
+#SBATCH --mem-per-cpu 32G
+#SBATCH --cpus-per-task=12
+
+# TODO: set num of cpu properly
+
+#SBATCH --gres=gpu:1
+#SBATCH --constraint='kepler|pascal|volta'
+
+# set -x # print all output to log file
+
+# load environment
+module purge
+module load anaconda3
+source activate edward
+
+which python
+# env
+
+# copy tfrecords files
+cd /scratch/cs/ai_croppro/
+mkdir /tmp/$SLURM_JOB_ID                                    # get a directory where you will send all output from your program
+cp 2_data/05_images_masked/*.tfrecord /tmp/$SLURM_JOB_ID    # copy tfrecords files to temporary directory
+cp 2_data/05_images_masked/*.txt /tmp/$SLURM_JOB_ID         # copy other necessary files to temporary directory
+                                                            # run python script with temporary directory as input for the images
+
+python 3_code/tf_vae_tfrecords.py -c triton --data_path /tmp/$SLURM_JOB_ID/
