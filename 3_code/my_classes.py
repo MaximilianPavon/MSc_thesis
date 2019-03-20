b975ac7b315796e1d627593461d1b0796d1b834c
MaximilianProll <maximilian.proll@hotmail.de>
2019-03-20 17:38:26 +0200

    VAE for the 64x64x1 images

--- 3_code/my_classes.py
+++ 3_code/my_classes.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os, glob
 import imageio
 from skimage import exposure
+import numpy as np
 
 
 class Monitor(Thread):
@@ -83,6 +84,64 @@ class MyCallbackDecoder(tf.keras.callbacks.Callback):
         # os.system('rm image_at_epoch_0*')  # optional clean up - not adapted yet
         pass
 
+class MyCallbackDecoder_NDVI(tf.keras.callbacks.Callback):
+    def __init__(self, decoder, log_dir, num_examples_to_generate=16, log_freq=10):
+        super(MyCallbackDecoder_NDVI, self).__init__()
+        self.decoder = decoder
+        self.log_dir = log_dir
+        self.num_examples_to_generate = num_examples_to_generate
+        self.latent_dim = self.decoder.input[0].shape.dims[0].value
+        self.log_freq = log_freq
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        # keeping the random vector constant for generation (prediction) so
+        # it will be easier to see the improvement.
+        self.random_vector_for_generation = tf.random_normal(shape=[self.num_examples_to_generate, self.latent_dim])
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        if epoch % self.log_freq == 0:
+            predictions = self.decoder.predict(self.random_vector_for_generation, steps=1)
+            fig = plt.figure(figsize=(4, 4))
+
+            for i in range(predictions.shape[0]):
+                plt.subplot(4, 4, i + 1)
+                im = predictions[i][:, :, 0]
+                im = exposure.rescale_intensity(im)
+                plt.imshow(im, cmap='gray')
+                plt.axis('off')
+
+            # tight_layout minimizes the overlap between 2 sub-plots
+            plt.savefig(
+                os.path.join(self.log_dir, 'image_at_epoch_{:04d}.png'.format(epoch)),
+                dpi=100, bbox_inches='tight')
+            # plt.show()
+            plt.close('all')
+            pass
+        else:
+            pass
+
+    def on_train_end(self, logs=None):
+        # merge individual images to one .gif
+        with imageio.get_writer(os.path.join(self.log_dir, 'decoded.gif'), mode='I') as writer:
+            filenames = glob.glob(os.path.join(self.log_dir, 'image*.png'))
+            filenames = sorted(filenames)
+            last = -1
+            for i, filename in enumerate(filenames):
+                frame = 2 * (i ** 0.5)
+                if round(frame) > round(last):
+                    last = frame
+                else:
+                    continue
+                image = imageio.imread(filename)
+                writer.append_data(image)
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+        # os.system('rm image_at_epoch_0*')  # optional clean up - not adapted yet
+        pass
+
+
 
 class MyCallbackCompOrigDecoded(tf.keras.callbacks.Callback):
     def __init__(self, log_dir, dataset, num_examples=10, log_freq=10):
@@ -128,6 +187,70 @@ class MyCallbackCompOrigDecoded(tf.keras.callbacks.Callback):
                 decoded_im = predictions[i][:, :, [3, 2, 1]]
                 decoded_im = exposure.rescale_intensity(decoded_im)
                 plt.imshow(decoded_im)
+                plt.axis('off')
+
+            # tight_layout minimizes the overlap between 2 sub-plots
+            plt.savefig(
+                os.path.join(self.log_dir, 'image_at_epoch_{:04d}.png'.format(epoch)),
+                dpi=100, bbox_inches='tight')
+            # plt.show()
+            plt.close('all')
+            pass
+        else:
+            pass
+
+    def on_train_end(self, logs=None):
+        # merge individual images to one .gif
+        with imageio.get_writer(os.path.join(self.log_dir, 'comparison_orig_decoded.gif'), mode='I') as writer:
+            filenames = glob.glob(os.path.join(self.log_dir, 'image*.png'))
+            filenames = sorted(filenames)
+            last = -1
+            for i, filename in enumerate(filenames):
+                frame = 2 * (i ** 0.5)
+                if round(frame) > round(last):
+                    last = frame
+                else:
+                    continue
+                image = imageio.imread(filename)
+                writer.append_data(image)
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+        # os.system('rm image_at_epoch_0*')  # optional clean up - not adapted yet
+        pass
+
+class MyCallbackCompOrigDecoded_NDVI(tf.keras.callbacks.Callback):
+    def __init__(self, log_dir, X, num_examples=10, log_freq=10):
+        super(MyCallbackCompOrigDecoded_NDVI, self).__init__()
+        self.log_dir = log_dir
+        self.X = X
+        self.num_examples = num_examples
+        self.log_freq = log_freq
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        # select num_examples from X and store arrays to keep them constant over all epochs
+        self.orig_images = self.X[np.random.choice(self.X.shape[0], self.num_examples, replace=False), :, :, :]
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        if epoch % self.log_freq == 0:
+            predictions = self.model.predict(self.orig_images, steps=1)
+
+            fig = plt.figure(figsize=(self.num_examples, 2))
+
+            for i in range(self.num_examples):
+                # first row (1..... num_examples) holds the original images
+                plt.subplot(2, self.num_examples, i + 1)
+                orig_im = self.orig_images[i][:, :, 0]
+                orig_im = exposure.rescale_intensity(orig_im)
+                plt.imshow(orig_im, cmap='gray')
+                plt.axis('off')
+
+                # second row (1 + num_examples ...... 2 * num_examples) shows the decoded images
+                plt.subplot(2, self.num_examples,  i + self.num_examples + 1)
+                decoded_im = predictions[i][:, :, 0]
+                decoded_im = exposure.rescale_intensity(decoded_im)
+                plt.imshow(decoded_im, cmap='gray')
                 plt.axis('off')
 
             # tight_layout minimizes the overlap between 2 sub-plots
---
+++ 3_code/vae_64.py
@@ -0,0 +1,337 @@
+from my_functions import get_available_gpus, sampling, plot_latent_space, create_dataset, get_device_id, get_OS
+from my_classes import MyCallbackDecoder_NDVI, MyCallbackCompOrigDecoded_NDVI
+import tensorflow as tf
+import datetime
+import argparse
+import sys
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+op_sys = get_OS()
+if op_sys == 'Darwin':
+    # fix for macOS issue regarding library import error:
+    # OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.
+    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    req_grp = parser.add_argument_group(title='required arguments')
+    req_grp.add_argument("-c", "--computer", help="Specify computer: use \'triton\', \'mac\' or \'workstation\'.",
+                         required=True)
+    parser.add_argument("-p", "--project_path", help="Specify project path, where the project is located.")
+    parser.add_argument("-d", "--data_path",
+                        help="Specify path, where the data is located. E.g. /tmp/$SLURM_JOB_ID/05_images_masked/ ")
+    parser.add_argument("-m", "--model", help="Load compiled models (.hdf5 file) saved by model.save(filepath). Path "
+                                              "until parent directory: e.g \'4_runs/logging/models/")
+    parser.add_argument("-w", "--weights", help="Load trained weights (.h5 file) saved by model.save_weights(filepath)."
+                                                "Path until parent directory: e.g \'4_runs/logging/weights/")
+    parser.add_argument("--mse", action='store_true', help="Use mse loss instead of binary cross entropy (default)")
+    parser.add_argument('-e', "--epochs", type=int, help="Specify the number of training epochs")
+    parser.add_argument('-z', "--latent_dim", type=int, help="Specify the dimensionality of latent space")
+    parser.add_argument("--n_conv", type=int, help="Specify the number of convolutional layers.")
+    parser.add_argument("--batch_normalization", action='store_true', default=False,
+                        help="Specify if batch normalizations shall be applied. Default False.")
+    parser.add_argument("--param_alternation", type=str,
+                        help="When looping over certain hyper parameters, this flag adds the hyper parameter of "
+                             "interest to the fron of the hyper parameter string.")
+    parser.add_argument("--debug", action='store_true', help="Run in debug mode - reduces epochs and steps_per_epoch")
+    args = parser.parse_args()
+
+    if not args.project_path:
+        if args.computer == 'triton':
+            args.project_path = '/scratch/cs/ai_croppro'
+        elif args.computer == 'mac':
+            args.project_path = '/Users/maximilianproll/Dropbox (Aalto)/'
+        elif args.computer == 'workstation':
+            args.project_path = '/m/cs/scratch/ai_croppro'
+        else:
+            sys.exit('Please specify the computer this programme runs on using \'triton\', \'mac\' or \'workstation\'')
+
+    if args.model and not args.project_path in args.model:
+
+        if '..' in args.model:
+            # remove leading '..'
+            args.model = '/'.join(args.model.split('/')[1:])
+
+        args.model = os.path.join(args.project_path, args.model)
+
+    if args.weights and not args.project_path in args.weights:
+
+        if '..' in args.weights:
+            # remove leading '..'
+            args.weights = '/'.join(args.weights.split('/')[1:])
+
+        args.weights = os.path.join(args.project_path, args.weights)
+
+    print('available GPUs:')
+    get_available_gpus()
+    # gpu_device_ID = get_device_id(gpu_pci_bus_id)
+
+    # Parameters
+    path_to_data = args.data_path if args.data_path else os.path.join(args.project_path, '2_data/data_for_comparision/')
+    im_dim = (64, 64)
+    n_channels = 1
+    input_shape = (im_dim[0], im_dim[1], n_channels)
+    n_Conv = args.n_conv if args.n_conv else 3
+    kernel_size = 3
+    filters = 10
+    latent_dim = args.latent_dim if args.latent_dim else 64
+    epochs = args.epochs if args.epochs else 100
+    batch_normalization = args.batch_normalization
+    loss_fct = 'MSE' if args.mse else 'X-Ent'
+    n_parallel_readers = 4
+    use_bias = not batch_normalization  # if batch_normalization is used, a bias term can be omitted
+    batch_size = 16
+
+    # load np arrays
+    X = np.load(os.path.join(path_to_data, 'X64.npy'))
+    y = np.load(os.path.join(path_to_data, 'Y64.npy'))
+
+    # compute NDVI value for X from the 13 channels
+    # NDVI = = (B08 - B04) / (B08 + B04)
+    # raw channels are: [B01,B02,B03,B04,B05,B06,B07,B08,B8A,B09,B10,B11,B12]
+
+    X_NDVI = []
+    for x_i in X:
+        # ndvi_i = np.empty(input_shape, dtype=np.float32)
+        # check = np.logical_and(np.logical_or(x_i[7] > 0, x_i[3] > 0), x_i[7] - x_i[3] > 0)
+        check = x_i[7] - x_i[3]!= 0
+        ndvi_i = np.where(check, (x_i[7] - x_i[3]) / (x_i[7] - x_i[3]), 0)
+        ndvi_i = np.reshape(ndvi_i, input_shape)
+        X_NDVI.append(ndvi_i)
+        del ndvi_i, check
+    X_NDVI = np.array(X_NDVI)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_NDVI, y, test_size = 0.20, random_state = 42)
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.50, random_state=42)
+
+    # save some RAM
+    del X, X_NDVI, y
+
+
+    # folder extension for bookkeeping
+    datetime_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    hparam_str = '64x64_'
+    hparam_str += args.param_alternation + '-' if args.param_alternation else ''
+    hparam_str += str(latent_dim) + 'z_'
+    hparam_str += str(n_Conv) + 'Conv_'
+    hparam_str += str(int(batch_normalization)) + 'BN_'
+    hparam_str += str(epochs) + 'ep_'
+    hparam_str += loss_fct + '_'
+    hparam_str += datetime_str
+
+    os.makedirs(os.path.join(args.project_path, '4_runs/plots/', hparam_str), exist_ok=True)
+
+    print('create graph / model')
+    # VAE model = encoder + decoder
+    # build encoder model
+    inputs = tf.keras.layers.Input(shape=input_shape, name='encoder_input')
+    x = inputs
+
+    if batch_normalization:
+        x = tf.keras.layers.BatchNormalization()(x)
+
+    for i in range(n_Conv):
+        x = tf.keras.layers.Conv2D(filters=filters if i < 5 else filters // 2,
+                                   kernel_size=kernel_size,
+                                   strides=2 if i < 5 else 1,
+                                   padding='same',
+                                   use_bias=use_bias)(x)
+
+        if batch_normalization:
+            x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.Activation(activation='relu')(x)
+
+    # shape info needed to build decoder model
+    shape = tf.keras.backend.int_shape(x)
+
+    x = tf.keras.layers.Flatten()(x)
+
+    # generate latent vector Q(z|X)
+    z_mean = tf.keras.layers.Dense(latent_dim, use_bias=use_bias)(x)
+    z_log_var = tf.keras.layers.Dense(latent_dim, use_bias=use_bias)(x)
+
+    if batch_normalization:
+        z_mean = tf.keras.layers.BatchNormalization()(z_mean)
+        z_log_var = tf.keras.layers.BatchNormalization()(z_log_var)
+
+    z_mean = tf.keras.layers.Activation(activation='relu', name='z_mean')(z_mean)
+    z_log_var = tf.keras.layers.Activation(activation='relu', name='z_log_var')(z_log_var)
+
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = tf.keras.layers.Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+
+    # instantiate encoder model
+    encoder = tf.keras.models.Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+    tf.keras.utils.plot_model(
+        encoder, to_file=os.path.join(args.project_path, '4_runs/plots/', hparam_str, 'vae_encoder.png'),
+        show_shapes=True)
+
+    # build decoder model
+    latent_inputs = tf.keras.layers.Input(shape=(latent_dim,), name='z_sampling')
+    x = latent_inputs
+
+    if batch_normalization:
+        x = tf.keras.layers.BatchNormalization()(x)
+
+    x = tf.keras.layers.Dense(shape[1] * shape[2] * shape[3], use_bias=use_bias)(x)
+
+    if batch_normalization:
+        x = tf.keras.layers.BatchNormalization()(x)
+
+    x = tf.keras.layers.Activation(activation='relu')(x)
+
+    x = tf.keras.layers.Reshape((shape[1], shape[2], shape[3]))(x)
+
+    for i in reversed(range(n_Conv)):
+        x = tf.keras.layers.Conv2DTranspose(filters=filters if i < 5 else filters // 2,
+                                            kernel_size=kernel_size,
+                                            strides=2 if i < 5 else 1,
+                                            padding='same',
+                                            use_bias=use_bias)(x)
+        if batch_normalization:
+            x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.Activation(activation='relu')(x)
+
+    # after the last Conv2DTranspose bring the output in the origninal shape
+    x = tf.keras.layers.Conv2DTranspose(filters=n_channels,
+                                        kernel_size=kernel_size,
+                                        padding='same',
+                                        use_bias=use_bias)(x)
+
+    if batch_normalization:
+        x = tf.keras.layers.BatchNormalization()(x)
+
+    outputs = tf.keras.layers.Activation(activation='relu', name='decoder_output')(x)
+
+    # instantiate decoder model
+    decoder = tf.keras.models.Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+    tf.keras.utils.plot_model(
+        decoder, to_file=os.path.join(args.project_path, '4_runs/plots/', hparam_str, 'vae_decoder.png'),
+        show_shapes=True)
+
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = tf.keras.models.Model(inputs, outputs, name='vae')
+
+    def KL_Div(y_true, y_pred):
+        kl_div = 1 + z_log_var - tf.keras.backend.square(z_mean) - tf.keras.backend.exp(z_log_var)
+        kl_div = tf.keras.backend.sum(kl_div, axis=-1)
+        kl_div *= -0.5
+        return kl_div
+
+    def recon_loss(y_true, y_pred):
+        if args.mse:
+            reconstruction_loss = tf.keras.losses.mse(
+                tf.keras.backend.flatten(y_true), tf.keras.backend.flatten(y_pred))
+        else:
+            reconstruction_loss = tf.keras.losses.binary_crossentropy(
+                tf.keras.backend.flatten(y_true), tf.keras.backend.flatten(y_pred))
+
+        reconstruction_loss *= im_dim[0] * im_dim[1]
+        return reconstruction_loss
+
+    def vae_loss(y_true, y_pred):
+        # VAE loss = mse_loss or xent_loss + kl_loss
+        reconstruction_loss = recon_loss(y_true, y_pred)
+        kl_div = KL_Div(y_true, y_pred)
+        vae_loss = tf.keras.backend.mean(reconstruction_loss + kl_div)
+        return vae_loss
+
+    rmsprop = tf.keras.optimizers.RMSprop(lr=0.00001)
+    vae.compile(optimizer=rmsprop, loss=vae_loss, metrics=[KL_Div, recon_loss])
+    vae.summary()
+    tf.keras.utils.plot_model(
+        vae, to_file=os.path.join(args.project_path, '4_runs/plots/', hparam_str, 'vae.png'),
+        show_shapes=True)
+
+    if args.weights:
+        print(f'loading weights from: {args.weights}')
+        vae = vae.load_weights(os.path.join(args.weights, 'vae.h5'))
+        encoder = encoder.load_weights(os.path.join(args.weights, 'encoder.h5'))
+        decoder = decoder.load_weights(os.path.join(args.weights, 'decoder.h5'))
+
+    elif args.model:
+        print(f'loading models from: {args.model}')
+        vae = tf.keras.models.load_model(os.path.join(args.model, 'vae.hdf5'),
+                                         custom_objects={'vae_loss': vae_loss,
+                                                         'KL_Div': KL_Div,
+                                                         'recon_loss': recon_loss})
+        encoder = tf.keras.models.load_model(os.path.join(args.model, 'encoder.hdf5'))
+        decoder = tf.keras.models.load_model(os.path.join(args.model, 'decoder.hdf5'))
+        print('models loaded')
+
+    else:
+        # train the autoencoder
+
+        # add callbacks for:
+        # - creating saving decoded image after log_freq epochs
+        # - TensorBoard logger
+        # - logging and for saving model checkpoints
+
+        mycb_comparison = MyCallbackCompOrigDecoded_NDVI(
+            log_dir=os.path.join(args.project_path, '4_runs/plots/', hparam_str, 'comparison'),
+            X=X_test, num_examples=10, log_freq=1)
+
+        mycb_decoder = MyCallbackDecoder_NDVI(
+            decoder, log_dir=os.path.join(args.project_path, '4_runs/plots/', hparam_str, 'decoder'),
+            num_examples_to_generate=16, log_freq=1)
+
+        tbCallBack = tf.keras.callbacks.TensorBoard(
+            log_dir=os.path.join(args.project_path, '4_runs/logging/TBlogs/' + hparam_str),
+            histogram_freq=0,  # TODO: fix error when setting histogram_freq > 0
+            batch_size=batch_size,
+            write_graph=True,
+            write_grads=False,
+            write_images=False,
+            embeddings_freq=0,
+            embeddings_layer_names=None,
+            embeddings_metadata=None,
+            embeddings_data=None,
+        )
+
+        model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join(args.project_path, '4_runs/logging/checkpoints/vae_' + hparam_str + '.hdf5'),
+            verbose=1,
+            save_best_only=True,
+            mode='min',
+            period=1,
+        )
+        callbacks_list = [
+            mycb_comparison,
+            mycb_decoder,
+            model_checkpoint,
+            tbCallBack
+        ]
+
+        print('start the training')
+        vae.fit(
+            x=X_train, y=X_train,
+            batch_size=batch_size,
+            validation_data=(X_val, X_val),
+            epochs=epochs if not args.debug else 3,
+            callbacks=callbacks_list,
+            workers=os.cpu_count(),
+            use_multiprocessing=True,
+        )
+        print('training done')
+
+        # save models and weights
+        dir_models = os.path.join(args.project_path, '4_runs/logging/models/', hparam_str)
+        os.makedirs(dir_models, exist_ok=True)
+        vae.save(os.path.join(dir_models, 'vae.hdf5'))
+        encoder.save(os.path.join(dir_models, 'encoder.hdf5'))
+        decoder.save(os.path.join(dir_models, 'decoder.hdf5'))
+
+        dir_weights = os.path.join(args.project_path, '4_runs/logging/weights/', hparam_str)
+        os.makedirs(dir_weights, exist_ok=True)
+        vae.save_weights(os.path.join(dir_weights, 'vae.h5'))
+        encoder.save_weights(os.path.join(dir_weights, 'encoder.h5'))
+        decoder.save_weights(os.path.join(dir_weights, 'decoder.h5'))
+        print('models and weights saved')
