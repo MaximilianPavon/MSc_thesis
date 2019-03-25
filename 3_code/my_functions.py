e9613dacbf0f42e8667aa3514e44a07e52deb3f7
MaximilianProll <maximilian.proll@hotmail.de>
2019-03-25 13:12:24 +0200

    preprocess for new plant species

--- 3_code/my_functions.py
+++ 3_code/my_functions.py
@@ -130,7 +130,7 @@ def get_device_util(deviceID):
     return gpu_util
 
 
-def preprocess_df(path_to_csv, path_to_data, colour_band, file_extension):
+def preprocess_df_Rehuohra(path_to_csv, path_to_data, colour_band, file_extension):
     df = pd.read_csv(path_to_csv)
     print('successfully loaded file: ', path_to_csv)
 
@@ -200,6 +200,149 @@ def preprocess_df(path_to_csv, path_to_data, colour_band, file_extension):
 
     print('data frame created with a total number of fields: ', df.shape[0])
     return df
+
+def preprocess_df_top4(path_to_csv, path_to_data, colour_band, file_extension):
+    # load data frame which contains field parcels and locations
+    df = pd.read_csv(path_to_csv)
+    print('successfully loaded file: ', path_to_csv)
+
+    # fill NaN as 0
+    df = df.fillna(0)
+
+    df = df.rename(index=str, columns={
+        'vuosi': 'YEAR',
+        'lohkonro': 'field parcel',
+        'tunnus': 'identifier',
+        'kasvikoodi': 'PLANT CODE',
+        'kasvi': 'PLANT',
+        'lajikekood': 'VARIETY CODE',
+        'lajike': 'VARIETY',
+        'pintaala': 'Property area',
+        'tays_tuho': 'full crop loss',
+        'ositt_tuho': 'partial crop loss'})
+
+    # remove duplicated entries in field parcel
+    print(df.shape[0] - len(np.unique(df['field parcel'])), 'duplicate entries')
+    df = df[df['field parcel'].duplicated() == False]
+    print(df.shape[0] - len(np.unique(df['field parcel'])), 'duplicate entries')
+
+    print('create new column: relative crop loss = crop loss / Property area')
+    df['full crop loss scaled'] = df['full crop loss'] / df['Property area']
+    df['partial crop loss scaled'] = df['partial crop loss'] / df['Property area']
+
+    print('categorise the data')
+
+    i_full = df[(df['full crop loss scaled'] == 1) & (df['partial crop loss scaled'] == 0)].index
+    df.loc[i_full, 'category'] = 'full'
+    del i_full
+
+    i_partial = df[(df['partial crop loss scaled'] == 1) & (df['full crop loss scaled'] <= 0)].index
+    df.loc[i_partial, 'category'] = 'partial'
+    del i_partial
+
+    i_noloss = df[(df['full crop loss scaled'] == 0) & (df['partial crop loss scaled'] == 0)].index
+    df.loc[i_noloss, 'category'] = 'noloss'
+    del i_noloss
+
+    i_fullandpartial = df[
+        (df['full crop loss scaled'] > 0) & (df['full crop loss scaled'] < 1) &
+        (df['partial crop loss scaled'] > 0) & (df['partial crop loss scaled'] < 1)
+        ].index
+    df.loc[i_fullandpartial, 'category'] = 'fullandpartial'
+    del i_fullandpartial
+
+    threshold = 0.5
+    i_anyloss = df[
+        (df['full crop loss scaled'] > threshold) | (df['partial crop loss scaled'] > threshold)
+        ].index
+    df.loc[i_anyloss, 'category_2'] = 'anyloss'
+    del i_anyloss
+
+    # count how many instances of each category each plant has
+    plants = []
+    numbers = []
+    fulls, partials, nolosses, fullandpartials = [], [], [], []
+    anylosses = []
+
+    for plant in np.unique(df['PLANT']):
+        plants.append(plant)
+        df_tmp = df[df['PLANT'] == plant]
+        numbers.append(len(df_tmp))
+        fulls.append(len(df_tmp[df_tmp['category'] == 'full']))
+        partials.append(len(df_tmp[df_tmp['category'] == 'partial']))
+        nolosses.append(len(df_tmp[df_tmp['category'] == 'noloss']))
+        fullandpartials.append(len(df_tmp[df_tmp['category'] == 'fullandpartial']))
+        anylosses.append(len(df_tmp[df_tmp['category_2'] == 'anyloss']))
+        del df_tmp
+
+    df_numbers = pd.DataFrame(
+        {'plant': plants,
+         'number': numbers,
+         'full': fulls,
+         'partial': partials,
+         'noloss': nolosses,
+         'fullandpartial': fullandpartials,
+         'anyloss': anylosses
+         })
+    del fulls, partials, nolosses, fullandpartials, anylosses, plants, numbers
+    df_numbers = df_numbers.sort_values(by=['anyloss'], ascending=False)
+
+    n = 5
+    print(f'select top {n} plants according to number of fields with any loss')
+    top_5_plants = df_numbers.sort_values(by=['anyloss'], ascending=False).plant[0:n].values
+
+    print('drop Rehuohra because it is already downloaded')
+    top_5_plants = np.delete(top_5_plants, np.where(top_5_plants == 'Rehuohra'))
+    print(top_5_plants)
+
+    print('reduce df to selected plants')
+    df = df[(df.PLANT == top_5_plants[0]) | (df.PLANT == top_5_plants[1]) | (df.PLANT == top_5_plants[2]) | (
+            df.PLANT == top_5_plants[3])]
+
+    print('reduce df to only those fields, which are either full, partial, fullandpartial or anyloss '
+          'and select the largest 6k fields with no loss')
+
+    i_losses, i_noloss6ks = [], []
+    for plant in top_5_plants:
+        df_plant = df[df.PLANT == plant]
+        i_losses += df_plant[(df_plant.category == 'full') | (df_plant.category == 'partial') | (
+                df_plant.category == 'fullandpartial') | (df_plant.category_2 == 'anyloss')].index.values.tolist()
+        i_noloss6ks += df_plant[
+                           (df_plant.category == 'noloss')
+                       ].sort_values(by=['Property area'], ascending=False).index[0:6000].values.tolist()
+
+    df = df[(df.index.isin(i_losses)) | df.index.isin(i_noloss6ks)]
+
+    print('check that all the remaining indeces have to be either in i_losses or in i_nolosses6ks')
+    if np.any(np.array([df.index.isin(i_losses), df.index.isin(i_noloss6ks)]), axis=0).all():
+        print(f'checking file existence for {len(df)} fields for {top_5_plants}')
+
+        col_list = ['field parcel', 'full crop loss scaled', 'partial crop loss scaled']
+
+        # print('trim data frame to:', col_list)
+        df = df[col_list]
+
+        # check if files for fields exist, if not, remove from data frame
+        # write relative path including colour band and file extension to dataframe for future usage
+        print('total number of fields before verifying file existence ', df.shape[0])
+        subfolders = ['dataset9/']
+        not_existing = []
+        for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+            for folder in subfolders:
+                file_full = os.path.join(path_to_data, folder) + row['field parcel'] + '_' + colour_band + file_extension
+                file_partial = folder + row['field parcel'] + '_' + colour_band + file_extension
+                if os.path.isfile(file_full):
+                    # df.at[index, 'full path'] = file_full
+                    df.at[index, 'partial path'] = file_partial
+                    break
+                elif folder == subfolders[-1]:
+                    not_existing.append(index)
+
+        df = df.drop(not_existing)
+
+        print('data frame created with a total number of fields: ', df.shape[0])
+    return df
+
 
 
 def split_dataframe(df, train_p, val_p, random_state=200):
--- 3_code/preprocess_df.py
+++ 3_code/preprocess_df.py
@@ -1,4 +1,4 @@
-from my_functions import preprocess_df
+from my_functions import preprocess_df_Rehuohra, preprocess_df_top4
 import os
 import argparse
 import sys
@@ -26,12 +26,14 @@ if __name__ == '__main__':
 
     params = {
         'path_to_csv': os.path.join(args.project_path, '2_data/01_MAVI_unzipped_preprocessed/MAVI2/2015/rap_2015.csv'),
-        'path_to_data': os.path.join(args.project_path, '2_data/05_images_masked/'),
+        'path_to_data': os.path.join(args.project_path, '2_data/03_data/'),
         'colour_band': 'BANDS-S2-L1C',
         'file_extension': '.tiff',
     }
 
-    df = preprocess_df(params['path_to_csv'], params['path_to_data'], params['colour_band'], params['file_extension'])
+    # df = preprocess_df_Rehuohra(params['path_to_csv'], params['path_to_data'], params['colour_band'], params['file_extension'])
+    df = preprocess_df_top4(params['path_to_csv'], params['path_to_data'], params['colour_band'],
+                                params['file_extension'])
 
     if not args.out_file:
         args.out_file = 'preprocessed.csv'
